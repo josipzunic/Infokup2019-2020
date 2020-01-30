f84a990709582ae2af04fa4ee8c19b476e4a6430
Helez359 <helenaaa359@gmail.com>
2020-01-30 23:23:38 +0100

    Created comunication with API

---
+++ backend/index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const port = 2000
+let pubchem = require("./node_modules/pubchem-access").domain("compound");
+let sentSmile = "[H]-[H]"
+ 
+
+
+app.get('/getCompound', (req, res) => {
+    pubchem
+    .setSmiles(sentSmile)
+    .getProperties(["IUPACName", "ExactMass", "MolecularFormula"])
+    .execute(function (data, status) {
+        res.send(data);
+});
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
