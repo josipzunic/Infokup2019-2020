7d1b858d5224c6f8eb1852b42cbf66447a609665
josipzunic <jzunic123@gmail.com>
2020-02-01 14:15:23 +0100

    Started working on communication

--- frontend/app/src/components/classPlugIn/classPlugIn.jsx
+++ frontend/app/src/components/classPlugIn/classPlugIn.jsx
@@ -5,20 +5,31 @@ import MoleculeInfo from '../../components/moleculeInfo/moleculeInfo';
 import styles from '../../pages/PlugInCompound/PlugInCompound.module.css';
 
 class PlugIn extends React.Component {
-    constructor() {
+    constructor(props) {
+        super(props);
         this.state = { apiResponse: '' };
+        this.callAPI = this.callAPI.bind(this);
 
     }
 
     callAPI() {
-        fetch('http://localhost:2000/getCompound')
+        let headers = new Headers();
+        
+        headers.append('Content-Type', 'application/json');
+        headers.append('Accept', 'application/json');
+        headers.append('Access-Control-Allow-Origin', 'http://localhost:2000');
+        headers.append('Access-Control-Allow-Credentials', 'true');
+        headers.append('GET', 'POST', 'OPTIONS');
+
+        fetch('http://localhost:2000/getCompound',
+            {
+                mode: 'no-cors',
+                method: 'GET',
+                headers: headers
+            })
             .then(res => res.text())
             .then(res => this.setState({ apiResponse: res }))
             .catch(err => err);
-    }
-
-    componentDidMount() {
-        this.callAPI();
     }
 
     render() {
@@ -26,7 +37,7 @@ class PlugIn extends React.Component {
             <div>
                 <div className={styles['container']}>
                     <div>
-                        <MoleculeBox />
+                        <MoleculeBox> {this.state.apiResponse} </MoleculeBox>
                         <MoleculeName />
                     </div>
                     <div className={styles['mol_info']}>
@@ -34,7 +45,7 @@ class PlugIn extends React.Component {
                     </div>
                 </div>
                 <div className={styles['button_div']}>
-                    <button> START </button>
+                    <button onClick={this.callAPI}> START </button>
                 </div>
             </div>
         );
