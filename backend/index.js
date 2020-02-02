9ffa0fa44e8d59cc15107156394a6f77851fe460
josipzunic <jzunic123@gmail.com>
2020-02-02 17:16:39 +0100

    Modified index.js

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,19 @@
-const express = require('express')
-const app = express()
-const port = 2000
-let pubchem = require("./node_modules/pubchem-access").domain("compound");
-let sentSmile = "[H]-[H]"
+const express = require('express');
+const app = express();
+const port = 2000;
+let pubchem = require('./node_modules/pubchem-access').domain('compound');
+let sentSmile = '[H]-[H]';
+let cors = require('cors');
+
+app.use(cors);
  
-
-
 app.get('/getCompound', (req, res) => {
     pubchem
     .setSmiles(sentSmile)
-    .getProperties(["IUPACName", "ExactMass", "MolecularFormula"])
+    .getProperties(['IUPACName', 'ExactMass', 'MolecularFormula'])
     .execute(function (data, status) {
         res.send(data);
 });
-})
+});
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
