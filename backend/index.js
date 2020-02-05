d5e2958da1483129428f0517987d777581dc1600
Helez359 <helenaaa359@gmail.com>
2020-02-05 16:27:30 +0100

    Finished backend for plugIn

--- backend/index.js
+++ backend/index.js
@@ -2,24 +2,18 @@ const express = require('express');
 const app = express();
 const port = 2000;
 let pubchem = require('./node_modules/pubchem-access').domain('compound');
-let sentSmile = '[H]-[H]';
 let cors = require('cors');
 
 app.use(cors());
- 
-app.post('/setSmile', (req, res) => {
-    sentSmile = req.body;
-    console.log(sentSmile);
-    res.sendStatus(200);
-});
 
-app.get('/getCompound', (req, res) => {
+
+app.get('/getCompound/', (req, res) => {
     pubchem
-    .setSmiles(sentSmile)
-    .getProperties(['IUPACName', 'ExactMass', 'MolecularFormula'])
-    .execute(function (data, status) {
-        res.send(data);
-});
+        .setSmiles(req.query[0])
+        .getProperties(['IUPACName', 'ExactMass', 'MolecularFormula'])
+        .execute(function (data, status) {
+            res.send(data);
+        });
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
