4d45bf689d0933a3d2e495ceda79be0743f86863
Helez359 <helenaaa359@gmail.com>
2020-02-07 01:58:37 +0100

    Changed way of communication

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,32 @@
 const express = require('express');
+const cors = require('cors');
+const PORT = 2000;
+const HOST = "0.0.0.0";
 const app = express();
-const port = 2000;
-let pubchem = require('./node_modules/pubchem-access').domain('compound');
-let cors = require('cors');
+const pubchem = require('./node_modules/pubchem-access').domain('compound');
 
 app.use(cors());
 
-
 app.get('/getCompound/', (req, res) => {
+    const moleculeFormule = req.query.molecule;
+
     pubchem
-        .setSmiles(req.query[0])
+        .setSmiles(moleculeFormule)
         .getProperties(['IUPACName', 'ExactMass', 'MolecularFormula'])
-        .execute(function (data, status) {
-            res.send(data);
+        .execute( (data, status) => {
+            if (status === 1) {
+                res.send(data);
+            } else {
+                res.status(404).send();
+            }
         });
+    
+    return;
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+function onServerListen() {
+    console.log(`App listening on ${HOST}:${PORT}`)
+}
+
+app.listen(PORT, HOST, onServerListen);
