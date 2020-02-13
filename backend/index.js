1af180bbb44c327d10ab78424bc58f3ef2a44116
josipzunic <jzunic123@gmail.com>
2020-02-13 15:38:46 +0100

    Arduino communication

--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,51 @@
-const express = require('express');
-const cors = require('cors');
-const PORT = 2000;
-const HOST = "0.0.0.0";
-const app = express();
-const pubchem = require('./node_modules/pubchem-access').domain('compound');
+const express = require('express');                                                     // Includes express libraries
+const cors = require('cors');                                                           // Includes cors libraries
+const PORT = 2000;                                                                      // Sets port on which the app will run on
+const HOST = '0.0.0.0';                                                                 // Sets host
+const app = express();                                                                  // Creates express aplication
+const pubchem = require('./node_modules/pubchem-access').domain('compound');            // Includes PubChem-access libraries for communication with PubChem REST API
+const serialport = require('serialport');
+const port = new serialport('COM3', {
+    baudRate: 9600,
+    parser: new serialport.parsers.Readline('\n'),
+});
 
-app.use(cors());
+app.use(cors());         //enables CORS thus allowing requests from aplications that run on diferent domain then the one this app runs on
 
-app.get('/getCompound/', (req, res) => {
-    const moleculeFormule = req.query.molecule;
+/**
+* Recieves a request for properties of a compound and sends them as response.
+*
+* Firstly the pubchems's method setSmiles() is called and as parameter moleculeFormula is forwarded.
+* Second, getProperties() method is forwarded an array of molecula's properties (strings) that we want to get.
+* After that the function that sends response to client is called 
+*
+* @param  {Object} req  HTTP request object that contains SMILES of a molecule which's properties will be sent.
+* @param  {Object} res  Respons object with methods used to give send response to client or to terminate the request-response cycle.
+* @return        Ends GET request.
+*/
+
+app.get('/getCompound', (req, res) => {
+
+    /**
+     * Saves the SMILES of a molecule sent by req parameter in its query property.
+     * @type {String}
+     */
+
+    const moleculeFormula = '';
+
+    port.on('open', function () {
+        port.on('data', function(data) {
+            moleculeFormula = data.toString();
+            port.close();
+        });
+    });
 
     pubchem
-        .setSmiles(moleculeFormule)
+        .setSmiles(moleculeFormula)
         .getProperties(['IUPACName', 'ExactMass', 'MolecularFormula', 'Charge', 'MonoisotopicMass', 'MolecularWeight'])
         .execute( (data, status) => {
-            if (status === 1) {
-                res.send(data);
+            if (status === 1) {     // If status is 1 the function has been exicutet with no problem
+                res.send(data);     // Data about molecule is sent to frontend ()
             } else {
                 res.status(404).send();
             }
@@ -24,8 +54,17 @@ app.get('/getCompound/', (req, res) => {
     return;
 });
 
+
 function onServerListen() {
     console.log(`App listening on ${HOST}:${PORT}`)
 }
+
+/**
+ * Returns an http.Server object
+ * 
+ * @param {Number} PORT The number of port on which the app will run on
+ * @param {String} HOST The host on which the app will run on
+ * @param {function} onServerListen Function that will be exicuted after the app runs
+ */
 
 app.listen(PORT, HOST, onServerListen);
